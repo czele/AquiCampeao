c31bb95b9c43ef138b40a08d1659650b943d8b47
Camila <camila_zele@hotmail.com>
2021-05-30 11:53:05 -0300

    botão salvar com route para listar da partida

--- src/app/component/partida/partida.component.ts
+++ src/app/component/partida/partida.component.ts
@@ -3,9 +3,7 @@ import { PartidaService } from 'src/app/service/partida.service';
 import { FormBuilder } from '@angular/forms';
 import { ClubeService } from 'src/app/service/clube.service';
 import { Clube } from 'src/app/model/clube.model';
-
-
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-partida',
@@ -15,7 +13,7 @@ import { Clube } from 'src/app/model/clube.model';
 
 export class PartidaComponent implements OnInit {
 
-  clubes:Clube[] = [];
+  clubes: Clube[] = [];
 
   form = this.formBuilder.group({
     idMandante: "",
@@ -24,14 +22,19 @@ export class PartidaComponent implements OnInit {
     idVisitante: "",
     data: "",
   })
+
   constructor(private formBuilder: FormBuilder,
     private _service: PartidaService,
-    private _serviceclube: ClubeService) { }
+    private _serviceclube: ClubeService,
+    private _router:Router) { }
+        
   ngOnInit(): void {
     this._serviceclube.listar().subscribe(result => this.clubes = result)
   }
   save() {
     console.log(this.form.value)
-    this._service.inserir(this.form.value).subscribe(response => console.log(response))
+    this._service.inserir(this.form.value).subscribe
+    (response => {console.log(response)
+    this._router.navigateByUrl('/partidalistar')})
   }
 }
